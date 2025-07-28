189a7fe9c74b2cba159d1ef88ff00920ecfbf76a
Simon Lindström <simon.tiger.lindstrom@gmail.com>
2025-07-28 14:56:35 +0200

    Add project files.

---
+++ Models/User.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino.Models
+{
+    public class User
+    {
+        public string Username { get; private set; }
+        public decimal Balance { get; private set; }
+
+        
+        public int GamesPlayed { get; private set; }
+        public int GamesWon { get; private set; }
+        public int GamesLost { get; private set; }
+
+        public User(string username, decimal startingBalance = 1000)
+        {
+            if (string.IsNullOrWhiteSpace(username))
+                throw new ArgumentException("Användarnamn kan inte vara tomt.");
+
+            Username = username;
+            Balance = startingBalance;
+            GamesPlayed = 0;
+            GamesWon = 0;
+            GamesLost = 0;
+        }
+
+        public bool AdjustBalance(decimal amount)
+        {
+            decimal newBalance = Balance + amount;
+
+            if (newBalance < 0)
+                return false; 
+
+            Balance = newBalance;
+            return true;
+        }
+
+        public void RecordGameResult(bool won)
+        {
+            GamesPlayed++;
+            if (won) GamesWon++;
+            else GamesLost++;
+        }
+
+        public override string ToString()
+        {
+            return $"{Username} | Saldo: {Balance} kr | Spelade: {GamesPlayed}, Vunna: {GamesWon}, Förlorade: {GamesLost}";
+        }
+    }
+}
+}
---
+++ Utils/Colours.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino.Utils
+{
+    public class Colours
+    {
+        private static readonly int[] redNumbers = {
+        1, 3, 5, 7, 9, 12, 14, 16, 18,
+        19, 21, 23, 25, 27, 30, 32, 34, 36
+    };
+
+        private static readonly int[] blackNumbers = {
+        2, 4, 6, 8, 10, 11, 13, 15, 17,
+        20, 22, 24, 26, 28, 29, 31, 33, 35
+    };
+
+        public static string GetColour(int number)
+        {
+            if (number == 0)
+                return "grön";
+            else if (redNumbers.Contains(number))
+                return "röd";
+            else if (blackNumbers.Contains(number))
+                return "svart";
+            else
+                return "okänd";
+        }
+    }
+}
