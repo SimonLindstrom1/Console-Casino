877e658fc0b3dd1dda287a2f0c2b7884b77d5f21
Simon Lindström <simon.tiger.lindstrom@gmail.com>
2025-07-29 15:43:37 +0200

    RNG

--- Core/RNG.cs
+++ Core/RNG.cs
@@ -8,5 +8,25 @@ namespace Casino.Core
 {
     public static class RNG
     {
+        private static readonly Random _random = new Random();
+
+        public static int Next(int min, int max)
+        {
+            return _random.Next(min, max);
+        }
+
+        public static bool CoinFlip()
+        {
+            return _random.Next(0, 2) == 1;
+        }
+
+        public static T Choice<T>(T[] items)
+        {
+            if (items == null || items.Length == 0)
+                throw new ArgumentException("Arrayen får inte vara tom.");
+
+            int index = _random.Next(0, items.Length);
+            return items[index];
+        }
     }
 }
